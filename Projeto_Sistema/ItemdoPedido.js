e9bb2e47a35db16fda8bfaf867eb9d887a18b6c8
Samuel <samuel1372822@gmail.com>
2025-09-09 15:57:14 -0300

    Delete Projeto_Sistema/ItemdoPedido.js

--- Projeto_Sistema/ItemdoPedido.js
+++
@@ -1,7 +0,0 @@
-class ItemDoPedido {
-    constructor(numPedido, codigoProduto, quantidade) {
-        this.numPedido = numPedido;
-        this.codigoProduto = codigoProduto;
-        this.quantidade = quantidade;
-    }
-}
