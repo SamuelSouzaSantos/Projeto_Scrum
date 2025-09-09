1d87ff5e1baa06f0327ddd709e42f984e1b89c70
Vinícius Abreu <viniciusabreu46311@hotmail.com>
2025-09-09 16:29:25 -0300

    Classe Cliente

---
+++ Projeto_Sistema/ClasseCliente/Cliente.js
@@ -0,0 +1,12 @@
+class Cliente{
+    constructor(codigo, nome, endereco, cidade, cep, uf, cgc, ie){
+        this.codigoCliente = codigo
+        this.nomeCliente = nome
+        this.endereco = endereco
+        this.cidade = cidade
+        this.cep = cep
+        this.uf = uf
+        this.cgc = cgc
+        this.ie = ie
+    }
+}
