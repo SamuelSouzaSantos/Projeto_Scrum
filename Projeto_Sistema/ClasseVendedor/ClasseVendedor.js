c0364b2db465f2ba9a6bf20a27522d4e38662aa2
DevMurilloJunior <murilloengenharias@gmail.com>
2025-09-09 17:18:43 -0300

    Create ClasseVendedor.js

---
+++ Projeto_Sistema/ClasseVendedor/ClasseVendedor.js
@@ -0,0 +1,8 @@
+export class Vendedor {
+    constructor(codigo, nome, salario, comissao){
+        this.codigoVendedor = codigo;
+        this.nomeVEndedor = nome;
+        this.salario = salario;
+        this.comissao = comissao;
+    }
+}
