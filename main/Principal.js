426b645375fcff6b09423bab246fd8cdfdc7fbc9
RafaelMendes1101 <rafaelmendessouza45@gmail.com>
2025-09-09 17:56:11 -0300

    Update Principal.js

--- main/Principal.js
+++ main/Principal.js
@@ -1,3 +1,5 @@
 import { Cliente } from "../Projeto_Sistema/ClasseCliente/Cliente.js";
+import { Pedido } from "../Projeto_Sistema/ClassePedido/Classepedido.js";
 
 const JoaoCliente = new Cliente("001", "João", "Asa Norte", "Brasília", "70968-574", "DF", "aaa", "bbb")
+const pedido1 = new Pedido ("101", "2025-09-09", "C55", "V55")
